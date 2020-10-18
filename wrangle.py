83df960eea7b182e844bab37cfb8c5749254482a
Kwame Taylor <maxkwametaylor@gmail.com>
2020-10-18 16:09:47 -0500

    drop cols and filter out multi units

--- wrangle.py
+++ wrangle.py
@@ -13,15 +13,23 @@ def get_connection(db, user=user, host=host, password=password):
 
 def get_zillow_data():
     '''
-    This function acquires the Zillow property values data with a SQL query to the Codeup database.
+    This function acquires the Zillow property Zestimate data with a SQL query to the Codeup database.
     '''
     sql_query = '''
-                SELECT properties_2017.parcelid, properties_2017.id, bathroomcnt, bedroomcnt, calculatedbathnbr, calculatedfinishedsquarefeet, fips, latitude, longitude, regionidcounty, roomcnt, yearbuilt, taxvaluedollarcnt, assessmentyear, propertycountylandusecode, propertylandusetypeid
+                SELECT * 
                 FROM properties_2017
-                JOIN predictions_2017 ON properties_2017.parcelid = predictions_2017.parcelid
-                WHERE transactiondate BETWEEN '2017-05-01' AND '2017-06-30'
-                AND propertylandusetypeid = '261' OR '262' OR '263' OR '264' OR '268' OR '273' OR '274' OR '275' OR '276' OR '279';
+                JOIN predictions_2017 using(parcelid)
+                LEFT JOIN airconditioningtype using(airconditioningtypeid)
+                LEFT JOIN architecturalstyletype using(architecturalstyletypeid)
+                LEFT JOIN buildingclasstype using(buildingclasstypeid)
+                LEFT JOIN heatingorsystemtype using(heatingorsystemtypeid)
+                LEFT JOIN propertylandusetype using(propertylandusetypeid)
+                LEFT JOIN storytype using(storytypeid)
+                LEFT JOIN typeconstructiontype using(typeconstructiontypeid)
+                WHERE latitude IS NOT NULL
+                AND longitude IS NOT NULL
                 '''
+#                 AND propertylandusetypeid = '261' OR '262' OR '263' OR '264' OR '268' OR '273' OR '274' OR '275' OR '276' OR '279';
     filename = "zillow_df.csv"
     if os.path.isfile(filename):
         df = pd.read_csv(filename)
@@ -36,16 +44,57 @@ def get_zillow_data():
         # Return the dataframe to the calling code
         return df
 
-def prepare_zillow():
+def prepare_zillow(df):
     '''
-    This function acquires and prepares the Zillow property values data.
-    Returns df; takes no arguments.
+    This function cleans up the Zillow property values data
+    by reducing features.
+    Returns df; takes df as argument.
     '''
-    df = get_zillow_data()
-
     # Drop features to create an MVP (first iteration)
-    df = df.drop(columns=['calculatedbathnbr', 'fips', 'latitude', 'longitude', 'regionidcounty', 'roomcnt', 'yearbuilt', 'assessmentyear', 'propertycountylandusecode', 'propertylandusetypeid'])
-    # Drop rows with NaNs
-    df = df.dropna()
+
+    # Just choosing features right now that sound unuseful to me to drop
+    df = df.drop(columns=['calculatedbathnbr', 'roomcnt', 'assessmentyear', 'propertycountylandusecode', 'storytypeid', 'typeconstructiontypeid', 'buildingclasstypeid', 'architecturalstyletypeid', 'heatingorsystemtypeid', 'id', 'id.1', 'basementsqft', 'airconditioningtypeid', 'architecturalstyledesc', 'buildingclassdesc', 'heatingorsystemdesc', 'storydesc', 'typeconstructiondesc', 'censustractandblock', 'rawcensustractandblock', 'propertylandusetypeid'])
+
+    # Drop redundant or unneeded square feet features
+    df = df.drop(columns=['finishedfloor1squarefeet', 'finishedsquarefeet12', 'finishedsquarefeet13', 'finishedsquarefeet15', 'finishedsquarefeet50', 'finishedsquarefeet6', 'lotsizesquarefeet', 'garagetotalsqft', 'lotsizesquarefeet', 'yardbuildingsqft17', 'yardbuildingsqft26'])
+
+    # Drop redundant or unneeded value and tax features
+    df = df.drop(columns=['structuretaxvaluedollarcnt', 'landtaxvaluedollarcnt', 'taxamount', 'taxdelinquencyflag', 'taxdelinquencyyear'])
+
+    # Drop more features based on how many nulls are present
+    # I'll do it manually this iteration but might choose to automate later
+    df = df.drop(columns=['buildingqualitytypeid', 'decktypeid', 'fireplacecnt', 'hashottuborspa', 'poolsizesum', 'pooltypeid10', 'pooltypeid2', 'pooltypeid7', 'threequarterbathnbr', 'fireplaceflag'])
+
+    # Drop features that could be useful in a later iteration but are just noise for the MVP
+    df = df.drop(columns=['garagecarcnt', 'fullbathcnt', 'poolcnt', 'propertyzoningdesc', 'regionidcity', 'regionidneighborhood', 'numberofstories'])
+
+    # Drop zip code related columns because zip codes are anonymized in this data
+    df = df.drop(columns=['regionidzip'])
+
+    # In next iteration of pipeline, I will turn transaction date into date type, but for now I will just drop that column.
+    df = df.drop(columns=['transactiondate'])
+
+    # In the next iteration of pipeline, I will impute airconditioningdesc and propertylandusedesc, and add the necessary documentation for that.
+    # For now I will drop airconditioningdesc, and I will use propertylandusedesc to make sure I only have single-unit properties.
+    df = df.drop(columns=['airconditioningdesc'])
+
+    # Set index to parcelid to make it easier to drop rows and for better readability
+    df = df.set_index('parcelid')
+
+    # Now I will filter out all properties that are not single-unit properties.
+
+    # Get names of indexes for non-Single Family Residental properties
+    cols_to_drop = df[df['propertylandusedesc'] != 'Single Family Residential'].index
+    # Delete these row indexes from dataFrame
+    df = df.drop(cols_to_drop)
+
+    # fillna on unitcnt with 1.00 and then drop rows that are not 1.00
+    df.unitcnt = df.unitcnt.fillna(1.00)
+    # Get names of indexes for non-Single Family Residental properties
+    cols_to_drop = df[df['unitcnt'] != 1.00].index
+    # Delete these row indexes from dataFrame
+    df = df.drop(cols_to_drop)
+
+    # Now to impute the remaining nulls.
 
     return df
