1f550041517fce1a31fdac0832f34e9ff7e3d359
Kwame Taylor <maxkwametaylor@gmail.com>
2020-10-20 11:43:59 -0500

    isolation forest anomaly detection

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+from math import sqrt
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, explained_variance_score, mean_absolute_error
+from sklearn.linear_model import LinearRegression, TweedieRegressor
+from sklearn.ensemble import IsolationForest
 
 def zillow_main_split(df):
     '''
@@ -80,3 +85,44 @@ def handle_outliers(df):
     '''This function WILL handle outliers. Implementation in later iteration.
     '''
     return df
+
+def isolation_forest(X_train, X_validate, X_test, y_train, y_validate, y_test):
+    # Isolation Forest
+    # identify outliers in the each dataset and train the models
+    iso = IsolationForest(max_samples=100, random_state=666, contamination=0.3)
+    yhat = iso.fit_predict(X_train)
+
+    iso_v = IsolationForest(max_samples=100, random_state=666, contamination=0.3)
+    yhat_v = iso.fit_predict(X_validate)
+
+    iso_t = IsolationForest(max_samples=100, random_state=666, contamination=0.3)
+    yhat_t = iso.fit_predict(X_test)
+
+    # select all rows that are not outliers
+    mask = yhat != -1
+    X_train, y_train = X_train[mask], y_train[mask]
+
+    mask = yhat_v != -1
+    X_validate, y_validate = X_validate[mask], y_validate[mask]
+
+    mask = yhat_t != -1
+    X_test, y_test = X_test[mask], y_test[mask]
+
+    # summarize the shape of the updated training dataset
+    print('New shapes of train data:', X_train.shape, y_train.shape)
+    # fit the model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    # evaluate the model
+    yhat = model.predict(X_validate)
+    # evaluate predictions
+    mae = mean_absolute_error(y_validate, yhat)
+    print('Mean Absolute error on validate data: %.3f' % mae)
+
+    return X_train, X_validate, X_test, y_train, y_validate, y_test
+
+def concat_dfs(train, X_train, y_train):
+    train = X_train
+    train['logerror'] = y_train
+    print('Shape:', train.shape)
+    return train
